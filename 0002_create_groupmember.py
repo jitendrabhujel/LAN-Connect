67b001cfc15be8c072b92488b8e7d89ccf18824c
Jitendra Bhujel <120593598+jitendrabhujel@users.noreply.github.com>
2025-04-18 20:39:58 +0545

    Add files via upload

---
+++ 0002_create_groupmember.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('app1', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='GroupMember',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('is_admin', models.BooleanField(default=False)),
+                ('joined_at', models.DateTimeField(auto_now_add=True)),
+                ('last_read', models.DateTimeField(auto_now=True)),
+                ('group', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='app1.group')),
+                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'unique_together': {('group', 'user')},
+            },
+        ),
+    ] 
